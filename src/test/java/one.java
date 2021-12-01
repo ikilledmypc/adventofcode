41e2adb78dc27bcd3d5be58d371b8b6d4b344ea0
tim <tim.smeets@timeseries.com>
2021-12-01 23:37:10 +0100

    add description

--- src/test/java/one.java
+++ src/test/java/one.java
@@ -1,6 +1,4 @@
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
@@ -8,6 +6,42 @@ import java.util.Iterator;
 import java.util.LinkedList;
 
 import org.junit.jupiter.api.Test;
+
+/**
+ * Considering every single measurement isn't as useful as you expected: there's just too much noise in the data.
+ *
+ * Instead, consider sums of a three-measurement sliding window. Again considering the above example:
+ *
+ * 199  A
+ * 200  A B
+ * 208  A B C
+ * 210    B C D
+ * 200  E   C D
+ * 207  E F   D
+ * 240  E F G
+ * 269    F G H
+ * 260      G H
+ * 263        H
+ *
+ * Start by comparing the first and second three-measurement windows. The measurements in the first window are marked A (199, 200, 208); their sum is 199 + 200 + 208 = 607. The second window is marked B (200, 208, 210); its sum is 618. The sum of measurements in the second window is larger than the sum of the first, so this first comparison increased.
+ *
+ * Your goal now is to count the number of times the sum of measurements in this sliding window increases from the previous sum. So, compare A with B, then compare B with C, then C with D, and so on. Stop when there aren't enough measurements left to create a new three-measurement sum.
+ *
+ * In the above example, the sum of each three-measurement window is as follows:
+ *
+ * A: 607 (N/A - no previous sum)
+ * B: 618 (increased)
+ * C: 618 (no change)
+ * D: 617 (decreased)
+ * E: 647 (increased)
+ * F: 716 (increased)
+ * G: 769 (increased)
+ * H: 792 (increased)
+ *
+ * In this example, there are 5 sums that are larger than the previous sum.
+ *
+ * Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
+ */
 
 public class one {
 	LinkedList<Integer> buffer = new LinkedList<>();
